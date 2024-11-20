70a803da3cdc5781abca31160792ad39863a365f
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-11-20 18:02:19 +0300

    added the first view

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def add (request):
+    return HttpResponse("hello world!")
