5aa9af1d4abf6d1d597cda05f0c49b00ca994344
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-11-20 17:58:45 +0300

    added some models for the practical�

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import User
+class Post(models.Model):
+    title = models.CharField(max_length=225)
+    created_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-# Create your models here.
+
+    def __str__(self):
+        return self.title
+
