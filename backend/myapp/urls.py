0312c62db593b788be7d6965da78d40883853064
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-11-21 19:23:02 +0300

    added some of all the emethond related to the post

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import Postview
+
 
 urlpatterns = [
+    path('', Postview.as_view(), name='post_list'),
+    path('/<int:pk>', Postview.as_view(), name='post_detail'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -4,8 +4,10 @@ from rest_framework import viewsets
 from rest_framework import status
 from .models import post
 from .serializers import PostSerializers
+from django.shortcuts import  get_object_or_404
 
-class Post(viewsets.ModelViewSet):
+
+class Postview(viewsets.ModelViewSet):
     queryset = post.objects.all()
     serializer_class = PostSerializers
     permission_classes = [permissions.AllowAny]
@@ -18,4 +20,21 @@ class Post(viewsets.ModelViewSet):
             return responce.Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return responce.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    def retrieve(self, request, pk=None):
+        post = get_object_or_404(post, pk=pk)
+        serializer = PostSerializers(post)
+        return responce.Response(serializer.data)
+    
+    def update(self, request, pk=None):
+        post = get_object_or_404(post, pk=pk)
+        serializer = PostSerializers(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return responce.Response(serializer.data)
+        return responce.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        post = get_object_or_404(post, pk=pk)
+        post.delete()
+        return responce.Response(status=status.HTTP_204_NO_CONTENT)
+    
