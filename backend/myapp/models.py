83c503c37fa7ca4fc28c1b7baf6d1ff534226ea3
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-11-23 14:44:56 +0300

    added the castomization of the models

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=225)
     created_at = models.DateTimeField(auto_now_add= True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    author = models.CharField(max_length=225)
     def __str__(self):
         return self.title
     
