93a155ec3c181b980be4cf11c67a05e7c586732e
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-11-28 14:52:38 +0300

    Add signal for sending welcome email after user registration

--- backend/myapp/apps.py
+++ backend/myapp/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from . import signals
 
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
+
+    def ready(self):
+        import myapp.signals 
