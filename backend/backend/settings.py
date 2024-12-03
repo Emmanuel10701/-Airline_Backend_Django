14a86ab16849b11950a1ee61d14e6e5ac7821f62
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-12-03 19:34:16 +0300

    added some of the api views for authentication in django using the simple jwt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,7 +81,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 
+}
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
