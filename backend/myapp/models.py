eda9355e4830dac7587da8b8cde3209a44fb0833
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-12-06 19:53:42 +0300

    added the profile model in the system

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import User
 
 class Post(models.Model):
     title = models.CharField(max_length=225)
@@ -17,3 +18,12 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"Message from {self.name} - {self.subject}"
+class Profile(models.Model):
+    User = models.ForeignKey(ondelete = models.CASCADE)
+    age = models.Intergerfile(Default = 0)
+    isVerified = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.User.username
+    
