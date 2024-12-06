d0f21f326e997fba57dd60252e72ecc8a53cd4d1
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-12-06 20:06:10 +0300

    added the urls configuration  because of the change of the additionsl infomation in the models of Profile

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -5,6 +5,7 @@ from .views import (
     RetrievePostAPIView,
     UpdatePostAPIView,
     DeletePostAPIView,
+    ProfileApi
 )
 from .view import home
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -13,7 +14,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('contact/', ContactAPIView.as_view(), name='contact'),
-
+    path('profile/', ProfileAPI.as_view(), name='profile'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('home/', name='home'),
